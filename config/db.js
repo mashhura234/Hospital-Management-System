9754d9d8f4b2fcb37d0119d21333fd6d34f235bc
Mashhura Nasrin Monika <mashhura.mu@gmail.com>
2026-04-16 18:32:00 +0600

    Database connected to SQL Server

--- config/db.js
+++ config/db.js
@@ -2,13 +2,15 @@ const sql = require('mssql');
 require('dotenv').config();
 
 const config = {
-    user: process.env.DB_USER,
+  user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
+    server: 'localhost\\SQLEXPRESS',
     database: process.env.DB_NAME,
+    port: 1433,
     options: {
         encrypt: false,
-        trustServerCertificate: true
+        trustServerCertificate: true,
+        instanceName: 'SQLEXPRESS'
     }
 };
 
